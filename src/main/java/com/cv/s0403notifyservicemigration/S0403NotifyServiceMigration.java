0cb84bc1528036a267a99cebbf9f220904e427e0
ram <ram_r@live.com>
2025-04-14 13:12:04 +0400

    BASE

---
+++ src/main/java/com/cv/s0403notifyservicemigration/S0403NotifyServiceMigration.java
@@ -0,0 +1,13 @@
+package com.cv.s0403notifyservicemigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S0403NotifyServiceMigration {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S0403NotifyServiceMigration.class, args);
+    }
+
+}
